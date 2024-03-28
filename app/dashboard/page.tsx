47e3321c6e497749f02f1f643dddf3e5da85523b
matiascamba <matiezequielcamba@gmail.com>
2024-03-28 19:27:36 -0300

    Next js tutorial

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+/* export default function Dashboard() {
+    return <p>Dashboard Page</p>
+} */
+
+// IGUAL A ↓ !!!!!!!!!!!!!!
+ const Dashboard = () => {
+    return <p>DashBoard Page</p>
+}
+
+
+export default Dashboard;
+
